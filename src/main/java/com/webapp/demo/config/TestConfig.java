71496426c83baa353bf917defe1f0eb69454ec5a
JSampx <joao.sampaio.andrade@gmail.com>
2025-01-29 13:14:03 -0300

    Criado enum list para o OrderStatus

--- src/main/java/com/webapp/demo/config/TestConfig.java
+++ src/main/java/com/webapp/demo/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.webapp.demo.config;
 
 import com.webapp.demo.entities.Order;
 import com.webapp.demo.entities.User;
+import com.webapp.demo.entities.enums.OrderStatus;
 import com.webapp.demo.repositories.OrderRepository;
 import com.webapp.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "Alex", "99999998", "alex@gmail.com", "654321");
 
 
-        Order o1 = new Order(null, Instant.parse("2025-01-28T19:23:01Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2025-01-28T20:03:11Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2025-01-29T10:13:10Z"), u2);
+        Order o1 = new Order(null, Instant.parse("2025-01-28T19:23:01Z"), u1, OrderStatus.PAID);
+        Order o2 = new Order(null, Instant.parse("2025-01-28T20:03:11Z"), u2, OrderStatus.WAITING_PAYMENT);
+        Order o3 = new Order(null, Instant.parse("2025-01-29T10:13:10Z"), u2, OrderStatus.WAITING_PAYMENT);
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
