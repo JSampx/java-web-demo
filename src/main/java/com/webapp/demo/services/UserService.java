bddbb92c4caaaeb6d7dd1092f7b14de3ebd590e0
JSampx <joao.sampaio.andrade@gmail.com>
2025-01-31 16:46:50 -0300

    Exceptions personalizadas

--- src/main/java/com/webapp/demo/services/UserService.java
+++ src/main/java/com/webapp/demo/services/UserService.java
@@ -3,6 +3,7 @@ package com.webapp.demo.services;
 import com.webapp.demo.entities.User;
 import com.webapp.demo.repositories.UserRepository;
 
+import com.webapp.demo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
@@ -22,7 +23,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 
     }
 
