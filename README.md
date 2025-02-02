baa5afb47858efdabd6acb1a769d5994d875f3e0
João Sampaio <joao.sampaio.andrade@gmail.com>
2025-02-02 17:24:41 -0300

    Alterações no README.md

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Projeto web em Java + Spring Boot
 
 ## Baseado nas aulas do prof Nélio Alves
+
+
+Projeto de api rest usando framework Java Spring Boot de um app simulando um serviço de pedidos.
--- src/main/java/com/webapp/demo/services/UserService.java
+++ src/main/java/com/webapp/demo/services/UserService.java
@@ -5,6 +5,7 @@ import com.webapp.demo.repositories.UserRepository;
 
 import com.webapp.demo.services.exceptions.DatabaseException;
 import com.webapp.demo.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.dao.DataIntegrityViolationException;
@@ -30,28 +31,34 @@ public class UserService {
 
     }
 
-    public User insert(User obj){
+    public User insert(User obj) {
         return repository.save(obj);
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+
+
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
         entity.setName(obj.getName());
         entity.setEmail(obj.getEmail());
         entity.setPhone(obj.getPhone());
-            }
+    }
 
-    public void delete(Long id){
-        try{
-        repository.deleteById(id);}
-        catch (EmptyResultDataAccessException e){
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
             throw new ResourceNotFoundException(id);
-        }catch (DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
             throw new DatabaseException(e.getMessage());
         }
     }
