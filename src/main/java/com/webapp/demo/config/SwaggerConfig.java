b2d52523f2c7960a9b9913ff427ebdba23070c5b
João Sampaio <joao.sampaio.andrade@gmail.com>
2025-05-09 14:52:47 -0300

    feat: Documentação da API com Swagger Open API
    # Conflicts:
    #	pom.xml
    #	src/main/resources/application-test.properties

---
+++ src/main/java/com/webapp/demo/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.webapp.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI().info(new Info()
+                .title("E-commerce Demo API")
+                .version("1.0.0")
+                .description("Documentação da API com OpenAPI - Swagger"));
+    }
+}
